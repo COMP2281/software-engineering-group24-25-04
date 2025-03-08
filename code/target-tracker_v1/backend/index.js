00b2556d7514dc039f89b82b08485dfa0751758f
Jasmeet <fvtx23@durham.ac.uk>
2025-03-08 18:08:57 +0000

    removed random managerDashboard page

--- code/target-tracker_v1/backend/index.js
+++ code/target-tracker_v1/backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./routes/router.js')
-const managerDashboard = require('./routes/managerDashboard.js')
 
 const app = express()
 
@@ -17,7 +16,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use('/', router)
-app.use('/managerDashboard', managerDashboard)
 
 const port = 4000
 const server = app.listen(port, () => {
