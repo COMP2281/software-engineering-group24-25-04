626701a81ed6569c987b44cb3bb3b1ea58ac87b8
Jasmeet <fvtx23@durham.ac.uk>
2025-03-08 19:00:53 +0000

    Added strong password requirements for signup

--- code/target-tracker_v1/frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ code/target-tracker_v1/frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,174 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import './LoginSignup.css';
+import { handleInput } from './LoginSignupInput';
+
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+
+const LoginSignup = ({ goToDashboard }) => {
+  const [action, setAction] = useState("Log In");
+
+  const handleLoginClick = async () => {
+    if (action !== "Log In") {
+      setAction("Log In");
+    } else {
+      const loginResult = await handleInput("Log In");
+
+      if (loginResult.success === true) {
+        if (loginResult.role === "manager") {
+          window.location.href = "/admin"; // Redirect managers to Admin Dashboard
+        } else {
+          window.location.href = "/user/dashboard"; // Redirect users to User Dashboard
+        }
+      } else {
+        alert("Login failed. Please check your credentials.");
+      }
+    }
+  };
+
+  const handleSignupClick = async () => {
+    if (action !== "Sign Up") {
+      setAction("Sign Up");
+    } else {
+      const signupResult = await handleInput("Sign Up");
+      if (signupResult === true) {
+        setAction("Log In");
+      }
+    }
+  };
+
+  const handleBackClick = () => {
+    if (action === "Forgot Password") {
+      setAction("Log In");
+    }
+  };
+
+  const handleResetClick = async () => {
+    if (action === "Forgot Password") {
+      const resetResult = await handleInput("Forgot Password");
+      if (resetResult === true) {
+        setAction("Log In");
+      }
+    }
+  };
+
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>{action}</div>
+        <div className='underline'></div>
+      </div>
+      
+      <div className='inputs'>
+        {(action === "Sign Up" || action === "Forgot Password") && (
+          <div className='input'>
+            <img src={user_icon} alt='' />
+            <input id="name" type='text' placeholder='Name' />
+          </div>
+        )}
+
+        <div className='input'>
+          <img src={email_icon} alt='' />
+          <input id="email" type='email' placeholder='Email ID' />
+        </div>
+
+        {action === "Forgot Password" ? (
+          <div>
+            <div className='input'>
+              <img src={password_icon} alt='' />
+              <input id="newPassword" type='password' placeholder='New Password' />
+            </div>
+            <div className='input'>
+              <img src={password_icon} alt='' />
+              <input id="newpassword" type='password' placeholder='Confirm New Password' />
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div className='input'>
+              <img src={password_icon} alt='' />
+              <input id="password" type='password' placeholder='Password' />
+            </div>
+            {action === "Sign Up" && (
+              <p className="password-requirements">
+                Password must be at least <b>8 characters</b> long and include:
+                <b> 1 uppercase</b>, <b>1 lowercase</b>, <b>1 number</b>, and <b>1 special character</b>.
+              </p>
+            )}
+          </div>
+        )}
+      </div>
+
+      {action === "Log In" && (
+        <div className="forgot-password">
+          Forgot Password? <span onClick={() => setAction("Forgot Password")}>Click Here</span>
+        </div>
+      )}
+
+      <div className="submit-container">
+        {action !== "Forgot Password" && (
+          <div className={action === "Login" ? "submit gray" : "submit"} onClick={handleSignupClick}>Sign Up</div>
+        )}
+        {action !== "Forgot Password" && (
+          <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={handleLoginClick}>Login</div>
+        )}
+        {action === "Forgot Password" && (
+          <div className="submit" onClick={handleResetClick}>Submit</div>
+        )}
+        {action === "Forgot Password" && (
+          <div className="back" onClick={handleBackClick}>Back</div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default LoginSignup;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, {useState} from 'react';
 import './LoginSignup.css';
 import {handleInput} from './LoginSignupInput';
 
@@ -43,32 +213,32 @@ const handleLoginClick = async () => {
     }
   };
 */
-  const handleSignupClick = async () => {
-    if (action !== "Sign Up"){
-        setAction("Sign Up");
+/*const handleSignupClick = async () => {
+if (action !== "Sign Up"){
+    setAction("Sign Up");
+}
+else {
+    const signupResult = await handleInput("Sign Up");
+    if (signupResult === true){
+        setAction("Log In"); 
     }
-    else {
-        const signupResult = await handleInput("Sign Up");
-        if (signupResult === true){
-            setAction("Log In"); 
-        }
-    }
-  };
+}
+};
 
-  const handleBackClick = async () => {
-    if (action === "Forgot Password"){
+const handleBackClick = async () => {
+if (action === "Forgot Password"){
+    setAction("Log In");
+}
+}
+
+const handleResetClick = async () => {
+if (action === "Forgot Password"){
+    const resetResult = await handleInput("Forgot Password");
+    if (resetResult === true){
         setAction("Log In");
     }
-    }
-  
-  const handleResetClick = async () => {
-    if (action === "Forgot Password"){
-        const resetResult = await handleInput("Forgot Password");
-        if (resetResult === true){
-            setAction("Log In");
-        }
-    }
-  }
+}
+}
 
   return (
     <div className='container'>
@@ -105,9 +275,13 @@ const handleLoginClick = async () => {
                     <img src={password_icon} alt='' />
                     <input id = "password" type='password' placeholder='Password'/>
                 </div>
-                
-
-                
+                {action === "Sign Up" && (
+                    <p className="password-requirements">
+                        Password must be at least <b>8 characters</b> long and include:
+                        <b> 1 uppercase</b>, <b>1 lowercase</b>, <b>1 number</b>, and <b>1 special character</b>.
+                    </p>
+                )}
+            </div> 
             )}
         </div>
             {action==="Log In" && (
@@ -132,10 +306,10 @@ const handleLoginClick = async () => {
 
             {/* <button className="go-to-dashboard" onClick={handleDashboardClick}>
              Go to Dashboard
-             </button> */}
+             </button> }*/
 
-        </div>
+        /*</div>
     );
 }
 
-export default LoginSignup
+export default LoginSignup*/
