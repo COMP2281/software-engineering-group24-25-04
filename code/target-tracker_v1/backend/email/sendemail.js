f5220c01f944cbe6188e788efbe08cd62cc1b22a
970754157 <buhongyu0702@163.com>
2025-04-25 13:40:32 +0100

    Replaced the valid mail server and replaced the correct mailbox test

--- code/target-tracker_v1/backend/email/sendemail.js
+++ code/target-tracker_v1/backend/email/sendemail.js
@@ -2,11 +2,16 @@ const nodemailer = require('nodemailer');
 
 // Create the transporter using MailerSend's SMTP
 const transporter = nodemailer.createTransport({
-    host: 'smtp.mailersend.net', // MailerSend SMTP host
-    port: 587, // Use port 587 for TLS
+    // host: 'smtp.mailersend.net', // MailerSend SMTP host
+    // port: 587, // Use port 587 for TLS
+    host: 'smtp.163.com',
+    port: 465,
+    secure: true,
     auth: {
-        user: 'MS_vDFEFM@trial-3zxk54v92kxgjy6v.mlsender.net', // Replace with your MailerSend SMTP username
-        pass: 'mssp.Iyq6dG8.neqvygm9xvdl0p7w.YQjjnPP'  // Replace with your MailerSend SMTP password
+        // user: 'MS_vDFEFM@trial-3zxk54v92kxgjy6v.mlsender.net', // Replace with your MailerSend SMTP username
+        // pass: 'mssp.Iyq6dG8.neqvygm9xvdl0p7w.YQjjnPP'  // Replace with your MailerSend SMTP password
+        user: 'buhongyu20020702@163.com',
+        pass: 'STn9Jq7dzWBGjkgp'
     },
     debug: true,
     tls: {
@@ -17,7 +22,8 @@ const transporter = nodemailer.createTransport({
 function sendEmail(send_email, message) {
     // Email options
     const mailOptions = {
-        from: 'donotreply-cerptargets@trial-3zxk54v92kxgjy6v.mlsender.net', // Sender address
+        // from: 'donotreply-cerptargets@trial-3zxk54v92kxgjy6v.mlsender.net', // Sender address
+        from:'buhongyu20020702@163.com',
         to: send_email,    // Recipient address
         subject: `CERP Target Reminder`, // Email subject
         text: `${message}`, // Email body (plain text)
